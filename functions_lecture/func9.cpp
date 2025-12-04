760aec1ea8ab02dbd102c845d2a7874314000717
Arunava-27 <drarunkundu22@gmail.com>
2025-12-05 01:11:49 +0530

    Implement prime checking function and update main to test user input

--- functions_lecture/func9.cpp
+++ functions_lecture/func9.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 // check if a number in prime or not.
-bool checkPrime(){}
+bool checkPrime(int n) {
+    if (n <= 1) return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
+
 
 int main(){
+    int n;
+    cin >> n;
+    bool result = checkPrime(n);
+    cout << boolalpha << result;
     return 0;
 }
